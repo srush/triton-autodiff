0c373b17a575b7e0ba86587c7443309db6c2cf7a
Sasha Rush <srush.research@gmail.com>
2024-01-21 22:02:58 -0500

    first setup

--- tests/test_autodiff.py
+++ tests/test_autodiff.py
@@ -31,15 +31,15 @@ def tre2(X, dX, dY):
     t2.src = st2
     return t1, t2
 
-def check(tr1, tr2, c):
-    x = torch.ones(16, requires_grad=True, device=torch.device(0))
-    y = torch.zeros(16).cuda()
+def check(tr1, tr2, c, x_shape=(16,), y_shape=(16,)):
+    x = torch.ones(*x_shape, requires_grad=True, dtype=float, device=torch.device(0))
+    y = torch.zeros(*y_shape, dtype=float).cuda()
     tr1[(1,)](x, y)
     y2 = c(x)
     assert torch.allclose(y, y2)
     y2.sum().backward()
-    x_grad = torch.zeros(16).cuda()
-    y_grad = torch.ones(16).float().cuda()
+    x_grad = torch.zeros(*x_shape, dtype=float).cuda()
+    y_grad = torch.ones(*y_shape, dtype=float).float().cuda()
     tr2[(1,)](x, x_grad, y_grad)
     assert torch.allclose(x.grad, x_grad)
 
@@ -56,7 +56,7 @@ def exp_check(x):
     return torch.exp(x)
 
 def test_run():
-    check("exptt", "expbacktt", exp_check)
+    check(*make_pair("exptt", "expbacktt"), exp_check)
 
 # Log
 def log(x):
@@ -71,7 +71,7 @@ def log_check(x):
     return torch.log(x)
 
 def test_run2():
-    check("logtt", "logbacktt", log_check)
+    check(*make_pair("logtt", "logbacktt"), log_check)
 
 
 # Complex
@@ -83,24 +83,87 @@ def compback(x, dx):
     pass
 compbacktt = grad(comp, compback)
 
-@triton.jit
-def tre1(X, Y):
-  r = tl.arange(0, 16)
-  x = tl.load(X + r)
-  y = comptt(x)
-  tl.store(Y + r, y)
-
-@triton.jit
-def tre2(X, dX, dY):
-  r = tl.arange(0, 16)
-  x = tl.load(X + r)
-  dy = tl.load(dY + r)
-  dx = compbacktt(x, dy)
-  tl.store(dX + r, dx)
-
 def comp_check(x):
     return torch.log(x) * torch.exp(x)
 
 def test_run3():
-    check(tre1, tre2, comp_check)
-test_run3()
+    check(*make_pair("comptt", "compbacktt"), comp_check)
+
+@triton.jit
+def ub1(X, Y):
+  r = tl.arange(0, 16)
+  r2 = tl.arange(0, 32)
+  x = tl.load(X + 16 * r2[:, None] + r)
+  y = triton_unbroadcast(x, tl.arange(0, 16))
+  tl.store(Y + r, y)
+
+@triton.jit
+def ub2(X, Y):
+  r = tl.arange(0, 16)
+  r2 = tl.arange(0, 32)
+  x = tl.load(X + 16 * r2[:, None] + r)
+  y = triton_unbroadcast(x, tl.arange(0, 32)[:, None])
+  tl.store(Y + r2[:, None], y)
+
+
+def test_unbroadcast():
+    x = torch.ones(32, 16, requires_grad=True, device=torch.device(0))
+    y = torch.zeros(16, requires_grad=True, device=torch.device(0))
+    ub1[(1,)](x, y)
+    assert torch.allclose(x.sum(0), y)
+    y = torch.zeros(32, requires_grad=True, device=torch.device(0))
+    ub2[(1,)](x, y)
+    assert torch.allclose(x.sum(1), y)
+# broadcast
+
+
+def comp2(x):
+    return tl.expand_dims(x, 1) * x
+comp2tt = triton.jit(comp2)
+
+def comp2back(x, dx):
+    pass
+comp2backtt = grad(comp2, comp2back)
+
+def comp2_check(x):
+    return x[:, None] * x
+
+@triton.jit
+def dcomp2dx(x, b_return):
+    _return2 = tl.expand_dims(x, 1)
+    bx = zeroslike(x)
+    b_return2 = zeroslike(_return2)
+
+    # Grad of: _return = _return2 * x
+    _b_return2 = triton_unbroadcast(b_return * x, _return2)
+    # _bx2 = triton_unbroadcast(b_return * _return2, x)
+    # b_return2 = add_grad(b_return2, _b_return2)
+    # bx = add_grad(bx, _bx2)
+
+    # # Grad of: _return2 = tl.expand_dims(x, 1)
+    # _bx = tl.sum(b_return2, 1)
+    # bx = add_grad(bx, _bx)
+    return bx
+
+
+@triton.jit
+def tr1(X, Y):
+  r = tl.arange(0, 16)
+  x = tl.load(X + r)
+  y = comp2tt(x)
+  tl.store(Y + 16 * r[:, None] + r, y)
+
+@triton.jit
+def tr2(X, dX, dY):
+  r = tl.arange(0, 16)
+  
+  r2 = tl.arange(0, 16)[:, None]
+  x = tl.load(X + r)
+  dy = tl.load(dY + 16 * r2 + r)
+  tl.static_print("shape", dy.shape)
+  dx = dcomp2dx(x, dy)
+  tl.static_print("shape", dx.shape)
+  tl.store(dX + r, dx)
+
+def test_run4():
+    check(tr1, tr2, comp2_check, x_shape=(16,), y_shape=(16, 16))
